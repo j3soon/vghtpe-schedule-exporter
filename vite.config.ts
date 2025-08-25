ea5981f4c7903e9c8eb5e33d22442143df60eed9
Johnson Sun <j3.soon777@gmail.com>
2025-08-26 02:50:48 +0800

    Add automated build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
   plugins: [react()],
 })
